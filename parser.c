a85b5fbd7b73835f171421f8c9301a1e84b733c6
Lukáš Vokráčko <lukas.vokracko@gmail.com>
2013-11-20 00:47:50 +0100

    Dokončení výrazů, rozdělení do více souborů, přidávání do stromů

--- parser.c
+++ parser.c
@@ -1,5 +1,82 @@
 #include "parser.h"
 
+int precedenceTable[][15] =
+{
+	{REDUCE,REDUCE,SHIFT,SHIFT,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,SHIFT,REDUCE,SHIFT,REDUCE},
+	{REDUCE,REDUCE,SHIFT,SHIFT,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,SHIFT,REDUCE,SHIFT,REDUCE},
+	{REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,SHIFT,REDUCE,SHIFT,REDUCE},
+	{REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,SHIFT,REDUCE,SHIFT,REDUCE},
+	{REDUCE,REDUCE,SHIFT,SHIFT,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,SHIFT,REDUCE,SHIFT,REDUCE},
+	{SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,SHIFT,REDUCE,SHIFT,REDUCE},
+	{SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,SHIFT,REDUCE,SHIFT,REDUCE},
+	{SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,SHIFT,REDUCE,SHIFT,REDUCE},
+	{SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,SHIFT,REDUCE,SHIFT,REDUCE},
+	{SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,REDUCE,REDUCE,SHIFT,REDUCE,SHIFT,REDUCE},
+	{SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,REDUCE,REDUCE,SHIFT,REDUCE,SHIFT,REDUCE},
+	{SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,EQUALSIGN,SHIFT,NOTHING},
+	{REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,NOTHING,REDUCE,NOTHING,REDUCE},
+	{REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,NOTHING,REDUCE,NOTHING,REDUCE},
+	{SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,FINISH,SHIFT,NOTHING}
+};
+
+LLFunction *LLRule[] =
+{
+	NULL,
+	argumentString,
+	argumentInteger,
+	argumentVar,
+	argumentNull,
+	argumentBoolean,
+	argumentDouble,
+	argumentList,
+	argumentArgumentList1,
+	argumentArgumentList2,
+	argumentList,
+	block,
+	semicolon,
+	commandVar,
+	commandReturn,
+	commandIf,
+	commandWhile,
+	closebrace,
+	commandList,
+	function,
+	functionCall,
+	expressionSemicolon,
+	closeparen,
+	paramList1,
+	closeparen,
+	paramList2,
+	program,
+	returnEnd,
+	returnExpression,
+	statementCommand,
+	statementFunction,
+	statementEnd,
+	expression,
+	expressionParen
+};
+
+/* @var tabulka která vrací pravidlo, to se použije pro indexaci funkcí LL tabulky */
+int LLTable[][36] =
+{
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,5,2,6,1,4,0,0,0,0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0,0,0,7,0,0,0,8,8,8,8,8,8,0,0,0,0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0,0,0,10,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,9,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,11,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,12,0,13,0,0,0,0,0,16,14,15,0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,18,0,18,0,0,0,0,0,18,18,18,0,0,0,0,0,17,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,19,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0,0,21,0,0,0,20,21,21,21,21,21,21,0,0,0,0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0,0,0,22,0,0,0,23,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0,0,0,24,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,25,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,26,0},
+	{0,0,0,0,0,0,0,0,0,0,0,28,0,0,27,0,28,28,28,28,28,28,0,0,0,0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,29,0,29,0,0,0,0,0,29,29,29,0,30,0,0,0,0,0,0,0,31},
+	{0,0,0,0,0,0,0,0,0,0,0,32,0,0,0,0,32,32,32,32,32,32,0,0,0,0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0,0,33,0,0,0,0,33,33,33,33,33,33,0,0,0,0,0,0,0,0,0,0,0,0,0}
+};
+
 //TODO napsat jako makra
 int isOperand(int tokenName)
 {
@@ -18,81 +95,87 @@ int isOperator(int tokenName)
 		tokenName == PLUS ||
 		tokenName == MINUS ||
 		tokenName == TIMES ||
+		tokenName == DIVISION ||
 		tokenName == DOT ||
-		tokenName == DIVISION;
+		tokenName == BIGGER ||
+		tokenName == LESSER ||
+		tokenName == BIGGEREQUAL ||
+		tokenName == LESSEREQUAL ||
+		tokenName == EQUAL ||
+		tokenName == NOTEQUAL;
 }
 
 
 
-void argumentString(tStack *s, tToken *t)
+void argumentString(tStack **s, tToken *t)
 {
 	stackPop(s);
 	stackPush(s, STRING);
 }
 
-void argumentInteger(tStack *s, tToken *t)
+void argumentInteger(tStack **s, tToken *t)
 {
 	stackPop(s);
 	stackPush(s, INTEGER);
 }
 
-void argumentVar(tStack *s, tToken *t)
+void argumentVar(tStack **s, tToken *t)
 {
 	stackPop(s);
 	stackPush(s, VAR);
 }
 
-void argumentNull(tStack *s, tToken *t)
+void argumentNull(tStack **s, tToken *t)
 {
 	stackPop(s);
 	stackPush(s, NULLV);
 }
 
-void argumentBoolean(tStack *s, tToken *t)
+void argumentBoolean(tStack **s, tToken *t)
 {
 	stackPop(s);
 	stackPush(s, BOOLEAN);
 }
 
-void argumentDouble(tStack *s, tToken *t)
+void argumentDouble(tStack **s, tToken *t)
 {
 	stackPop(s);
 	stackPush(s, DOUBLE);
 }
 
-void semicolon(tStack *s, tToken *t)
+void semicolon(tStack **s, tToken *t)
 {
 	stackPop(s);
 	stackPush(s, SEMICOLON);
 }
 
-void closebrace(tStack *s, tToken *t)
+void closebrace(tStack **s, tToken *t)
 {
 	stackPop(s);
 	stackPush(s, CLOSEBRACE);
 }
 
-void closeparen(tStack *s, tToken *t)
+void closeparen(tStack **s, tToken *t)
 {
 	stackPop(s);
 	stackPush(s, CLOSEPAREN);
 }
 
-void argumentList(tStack *s, tToken *t)
+void argumentList(tStack **s, tToken *t)
 {
 	stackPop(s);
 	stackPush(s, SEMICOLON);
 	stackPush(s, CLOSEPAREN);
 }
 
-void argumentArgumentList1(tStack *s, tToken *t)
+void argumentArgumentList1(tStack **s, tToken *t)
 {
 	stackPop(s);
 	stackPush(s, NARGUMENTLIST2);
 	stackPush(s, NARGUMENT);
 }
 
-void argumentArgumentList2(tStack *s, tToken *t)
+void argumentArgumentList2(tStack **s, tToken *t)
 {
 	stackPop(s);
 	stackPush(s, NARGUMENTLIST2);
@@ -100,14 +183,14 @@ void argumentArgumentList2(tStack *s, tToken *t)
 	stackPush(s, COMMA);
 }
 
-void block(tStack *s, tToken *t)
+void block(tStack **s, tToken *t)
 {
 	stackPop(s);
 	stackPush(s, NCOMMANDLIST);
 	stackPush(s, OPENBRACE);
 }
 
-void commandVar(tStack *s, tToken *t)
+void commandVar(tStack **s, tToken *t)
 {
 	stackPop(s);
 	stackPush(s, NFUNCTIONCALLEXPRESSION);
@@ -115,41 +198,41 @@ void commandVar(tStack *s, tToken *t)
 	stackPush(s, VAR);
 }
 
-void commandReturn(tStack *s, tToken *t)
+void commandReturn(tStack **s, tToken *t)
 {
 	stackPop(s);
 	stackPush(s, NRETURN);
 	stackPush(s, RETURN);
 }
 
-void commandIf(tStack *s, tToken *t)
+void commandIf(tStack **s, tToken *t)
 {
 	stackPop(s);
 	stackPush(s, NBLOCK);
 	stackPush(s, ELSE);
 	stackPush(s, NBLOCK);
 	stackPush(s, CLOSEPAREN);
-	stackPush(s, NEXPRESSION);
+	stackPush(s, NEXPRESSIONPAREN);
 	stackPush(s, OPENPAREN);
 	stackPush(s, IF);
 }
 
-void commandWhile(tStack *s, tToken *t)
+void commandWhile(tStack **s, tToken *t)
 {
 	stackPop(s);
 	stackPush(s, NBLOCK);
 	stackPush(s, CLOSEPAREN);
-	stackPush(s, NEXPRESSION);
+	stackPush(s, NEXPRESSIONPAREN);
 	stackPush(s, OPENPAREN);
 	stackPush(s, WHILE);
 }
 
-void commandList(tStack *s, tToken *t)
+void commandList(tStack **s, tToken *t)
 {
 	stackPush(s, NCOMMAND);
 }
 
-void function(tStack *s, tToken *t)
+void function(tStack **s, tToken *t)
 {
 	stackPop(s);
 	stackPush(s, NBLOCK);
@@ -159,7 +242,7 @@ void function(tStack *s, tToken *t)
 	stackPush(s, FUNCTION);
 }
 
-void functionCall(tStack *s, tToken *t)
+void functionCall(tStack **s, tToken *t)
 {
 	stackPop(s);
 	stackPush(s, NARGUMENTLIST1);
@@ -167,22 +250,21 @@ void functionCall(tStack *s, tToken *t)
 	stackPush(s, ID);
 }
 
-void expressionSemicolon(tStack *s, tToken *t)
+void expressionSemicolon(tStack **s, tToken *t)
 {
-	// DEBUG("expression vud");
 	stackPop(s);
 	stackPush(s, SEMICOLON);
 	stackPush(s, NEXPRESSION);
 }
 
-void paramList1(tStack *s, tToken *t)
+void paramList1(tStack **s, tToken *t)
 {
 	stackPop(s);
 	stackPush(s, NPARAMLIST2);
 	stackPush(s, VAR);
 }
 
-void paramList2(tStack *s, tToken *t)
+void paramList2(tStack **s, tToken *t)
 {
 	stackPop(s);
 	stackPush(s, NPARAMLIST2);
@@ -190,54 +272,64 @@ void paramList2(tStack *s, tToken *t)
 	stackPush(s, COMMA);
 }
 
-void program(tStack *s, tToken *t)
+void program(tStack **s, tToken *t)
 {
 	stackPop(s);
 	stackPush(s, NSTATEMENT);
 	stackPush(s, BEGIN);
 }
 
-void returnEnd(tStack *s, tToken *t)
+void returnEnd(tStack **s, tToken *t)
 {
 	stackPop(s);
 	stackPush(s, SEMICOLON);
 }
 
-void returnExpression(tStack *s, tToken *t)
+void returnExpression(tStack **s, tToken *t)
 {
 	stackPop(s);
 	stackPush(s, SEMICOLON);
 	stackPush(s, NEXPRESSION);
 }
 
-void statementCommand(tStack *s, tToken *t)
+void statementCommand(tStack **s, tToken *t)
 {
 	stackPush(s, NCOMMAND);
 }
 
-void statementFunction(tStack *s, tToken *t)
+void statementFunction(tStack **s, tToken *t)
 {
 	stackPush(s, NFUNCTION);
 }
 
-void statementEnd(tStack *s, tToken *t)
+void statementEnd(tStack **s, tToken *t)
 {
 	stackPop(s);
 	stackPush(s, END);
 }
 
-void expression(tStack *s, tToken *t)
+void expressionParen(tStack **s, tToken *t)
 {
-	DEBUG("expression");
+	// DEBUG("expressionParen");
+	// exit(1);
+	stackPop(s);
+	stackPush(s, SEMICOLON); //zarážka pro výrazy se závorkou
+	stackPush(s, NEXPRESSION);
+	expression(s, t);
+	stackPop(s);
+}
+
+void expression(tStack **s, tToken *t)
+{
 	stackPop(s);
 
 	int top, operation;
 	tStack *tmpStack = gmalloc(sizeof(tStack), free);
-	stackInit(tmpStack, 25);
+	stackInit(tmpStack, 8);
 
 	do
 	{
-		top = stackTopTerminal(s, tmpStack, true);
+		top = stackTopTerminal(s, &tmpStack, true);
 		operation = precedenceTable[top][isOperand(t->name) ? VALUE : t->name];
 		// printf("%d\n",top);
 		// printf("%d\n",t->name);
@@ -245,25 +337,30 @@ void expression(tStack *s, tToken *t)
 		// printf("%d", operation);
 		switch(operation)
 		{
-			case REDUCE: reduce(s,tmpStack);break;
-			case SHIFT: shift(s, tmpStack, t);break;
-			case EQUALSIGN: equalsign(s,tmpStack, t);break;
+			case REDUCE: reduce(s, &tmpStack);break;
+			case SHIFT: shift(s, &tmpStack, t);break;
+			case EQUALSIGN: equalsign(s, &tmpStack, t);break;
+			case FINISH:
+				stackTopTerminal(s, &tmpStack, true);
+				return;
+
 			default: printError(SYNTAXERR, SYNTAXERROR);
 		}
 	// DEBUG(tokenNames[stackTop(s)]);
 	// DEBUG(tokenNames[t->name]);
-	} while(stackTopTerminal(s, tmpStack, false) != SEMICOLON || t->name != SEMICOLON);
+	} while(stackTopTerminal(s, &tmpStack, false) != SEMICOLON || t->name != SEMICOLON);
 
-	stackTopTerminal(s, tmpStack, true);
+	stackTopTerminal(s, &tmpStack, true);
+	gfree(tmpStack);
 }
 
 
-void reduce(tStack *s,tStack *tmpStack)
+void reduce(tStack **s,tStack **tmpStack)
 {
 	stackPush(s, REDUCE);
 	stackPop(s);
 
-	while(!stackEmpty(tmpStack)) stackPush(s, stackPop(tmpStack));
+	while(!stackEmpty(*tmpStack)) stackPush(s, stackPop(tmpStack));
 
 	int top = stackPop(s);
 
@@ -323,114 +420,35 @@ void reduce(tStack *s,tStack *tmpStack)
 
 }
 
-void shift(tStack *s, tStack *tmpStack, tToken *t)
+void shift(tStack **s, tStack **tmpStack, tToken *t)
 {
 	// terminal = stackTopTerminal(s, tmpStack, true);
 
 	stackPush(s, SHIFT);
 
-	while(!stackEmpty(tmpStack)) stackPush(s, stackPop(tmpStack));
+	while(!stackEmpty(*tmpStack)) stackPush(s, stackPop(tmpStack));
 
 	stackPush(s, isOperand(t->name) ? VALUE : t->name );
 	getToken(f, t);
 }
 
-void equalsign(tStack *s,tStack *tmpStack, tToken *t)
+void equalsign(tStack **s,tStack **tmpStack, tToken *t)
 {
-	while(!stackEmpty(tmpStack)) stackPush(s, stackPop(tmpStack));
+	while(!stackEmpty(*tmpStack)) stackPush(s, stackPop(tmpStack));
 
 	stackPush(s, t->name);
 	getToken(f, t);
 }
 
-void stackInit(tStack *s, int size)
-{
-	s->top = -1;
-	s->max = size-1;
-	s->data = gmalloc(sizeof(int)*size, free);
-
-	if(s->data == NULL)
-	{
-		DEBUG("init");
-		printError(ALLOCERROR,INTERPRETERROR);
-	}
-}
-
-int stackPop(tStack *s)
-{
-	if(s->top == -1)
-	{
-		DEBUG("pop");
-		printError(STACKERROR, INTERPRETERROR);
-	}
-		printStack(s);
-
-	return s->data[s->top--];
-}
-
-void stackPush(tStack *s, int data)
-{
-	if(s->top == s->max)
-	{
-		DEBUG("push");
-		printError(STACKERROR, INTERPRETERROR);
-	}
-		printStack(s);
-
-	s->data[++(s->top)] = data;
-}
-
-int stackTop(tStack *s)
-{
-	if(s->top == -1)
-	{
-		DEBUG("top");
-		printError(STACKERROR, INTERPRETERROR);
-	}
-
-	return s->data[s->top];
-}
-
-int stackTopTerminal(tStack *s, tStack *tmpStack, bool useTmpStack)
-{
-	for(int i = s->top; i > -1; --i)
-	{
-		if(s->data[i] < NONTERMINALBORDER || s->data[i] >= PRECEDENCEBORDER) return s->data[i];
-		if(useTmpStack) stackPush(tmpStack, stackPop(s));
-	}
-
-	DEBUG("nenašel terminál v zásobníku");
-	printError(SYNTAXERR, SYNTAXERROR);
-	return 0;
-}
-
-
-int stackEmpty(tStack *s)
-{
-	return s->top == -1;
-}
-
-void printStack(tStack *s)
-{
-	int i = 0;
-
-	while(s->top - i >= 0) printf("%s ", tokenNames[s->data[i++]]);
-	printf("\n");
-}
-
 int main (int argc, char *argv[])
 {
 	bool breakParent = false;
+	int innerBraces;
 	LLFunction *LLCall;
-	// pokud spatny pocet parametru
+
 	if (argc!=2) printError(PARAMSERROR,INTERPRETERROR);
-
-	// pokus o otevreni zdrojaku
 	f=fopen(argv[1],"r");
-	// pripad nepodareneho otevreni
 	if (f==NULL) printError(SOURCECODEERR,INTERPRETERROR);
-
-	// kod otevren
 
 	// pokud nezacina < je to chyba
 	int c=fgetc(f);
@@ -439,42 +457,74 @@ int main (int argc, char *argv[])
 
 	ginit();
 	tToken *t = (tToken *) gmalloc(sizeof(tToken), free);
-	if(t == NULL) printError(ALLOCERROR, INTERPRETERROR);
-
 	t->content = (char *) gmalloc(40, free);
-	if(t->content == NULL) printError(ALLOCERROR, INTERPRETERROR);
-
 	tStack *stack = gmalloc(sizeof(tStack), free);
-	if(stack == NULL) printError(ALLOCERROR, INTERPRETERROR);
 	stackInit(stack, 40);
 
 	BSTF_Init(&functionTree);
 	actualFunction[0] = BSTF_Insert(&functionTree, "1");
+	BSTF_Print(functionTree);
+	gadd(functionTree, BSTF_Dispose);
 
 	// typ struktury tokenu
 	getToken(f, t);
-	stackPush(stack, NPROGRAM);
+	stackPush(&stack, NPROGRAM);
 
 	while(t->name != END && !stackEmpty(stack))
 	{
-		while(stackTop(stack) < NONTERMINALBORDER)
+		while(stackTop(&stack) < NONTERMINALBORDER)
 		{
-			DEBUG(tokenNames[t->name]);
 
-			if(stackPop(stack) != t->name)
+			if(stackPop(&stack) != t->name)
 			{
 				DEBUG("break parent konec");
 				breakParent = true;
 				// getToken(f, t);
 				break;
 			}
+
+			if(LLCall == commandVar && t->name == VAR)
+			{
+				printf("přidávám proměnnou: %s do funkce %s\n", t->content, actualFunction[0]->key);
+				BSTV_Insert(&(actualFunction[0]->variables), t->content);
+				BSTV_Print(actualFunction[0]);
+			}
+			else if(LLCall == function && t->name == ID)
+			{
+				actualFunction[1] = actualFunction[0];
+				actualFunction[0] = BSTF_Insert(&functionTree, t->content);
+				BSTF_Print(functionTree);
+			}
+			else if((LLCall == paramList1 || LLCall == paramList2) && t->name == VAR)
+			{
+				BSTV_Insert(&(actualFunction[0]->variables), t->content);
+				BSTV_Print(actualFunction[0]);
+			}
+			else if(LLCall == function && t->name == OPENBRACE)
+			{
+				innerBraces = 0;
+			}
+			else if(t->name == OPENBRACE)
+			{
+				DEBUG("přidávám {");
+				++innerBraces;
+			}
+			else if(t->name == CLOSEBRACE)
+			{
+				--innerBraces;
+				if(innerBraces == 0)
+				{
+					printf("vylézám z funkce %s", actualFunction[0]->key);
+					actualFunction[0] = actualFunction[1];
+				}
+			}
+
 			getToken(f, t);
 		}
 
 		if(breakParent) break;
-		// printf("%d\n", stackTop(stack)-NONTERMINALBORDER);
-		// printf("%d\n", t->name);
-		LLCall = LLRule[LLTable[stackTop(stack)-NONTERMINALBORDER][t->name]];
+
+		LLCall = LLRule[LLTable[stackTop(&stack)-NONTERMINALBORDER][t->name]];
 
 		if(LLCall == NULL)
 		{
@@ -482,10 +532,10 @@ int main (int argc, char *argv[])
 			break;
 		}
 
-		LLCall(stack, t);
+		LLCall(&stack, t);
 	}
 
-	if(stackEmpty(stack) || t->name != END || stackTop(stack) != END || stack->top != 0)
+	if(stackEmpty(stack) || t->name != END || stackTop(&stack) != END || stack->top != 0)
 	{
 		DEBUG("špatné ukončení programu");
 		printError(SYNTAXERR, SYNTAXERROR);
@@ -493,8 +543,6 @@ int main (int argc, char *argv[])
 
 	gfreeAll();
 	fclose(f);
-
-	printf("ok\n");
 
 	return 0;
 }
--- parser.h
+++ parser.h
@@ -10,8 +10,8 @@
 #include "error.h"
 #include "ial.h"
 #include "garbage.h"
-
-
+#include "debug.h"
+#include "stack.h"
 
 // enum pro navraty z funkce v parser.c
 enum tret
@@ -20,13 +20,6 @@ enum tret
 	ISNT=1,
 	ISOK=2,
 };
-
-typedef struct tStack
-{
-	int top;
-	int max;
-	int *data;
-} tStack;
 
 enum nonTerminals
 {
@@ -44,6 +37,7 @@ enum nonTerminals
 	NRETURN,
 	NSTATEMENT,
 	NEXPRESSION,
+	NEXPRESSIONPAREN,
 	E, // E pro precedenční analýzu
 };
 
@@ -52,7 +46,8 @@ enum precedenceValues
 	NOTHING = 200,
 	REDUCE,
 	SHIFT,
-	EQUALSIGN
+	EQUALSIGN,
+	FINISH,
 };
 
 int isFunction (FILE *f, tToken *t);
@@ -74,201 +69,54 @@ int isFunctionCall(FILE *f, tToken *t);
 
 
 //funkce pravidel
-void argumentString(tStack *s, tToken *t);
-void argumentInteger(tStack *s, tToken *t);
-void argumentVar(tStack *s, tToken *t);
-void argumentNull(tStack *s, tToken *t);
-void argumentBoolean(tStack *s, tToken *t);
-void argumentDouble(tStack *s, tToken *t);
-void semicolon(tStack *s, tToken *t);
-void closebrace(tStack *s, tToken *t);
-void closeparen(tStack *s, tToken *t);
-void argumentList(tStack *s, tToken *t);
-void argumentArgumentList1(tStack *s, tToken *t);
-void argumentArgumentList2(tStack *s, tToken *t);
-void block(tStack *s, tToken *t);
-void commandVar(tStack *s, tToken *t);
-void commandReturn(tStack *s, tToken *t);
-void commandIf(tStack *s, tToken *t);
-void commandWhile(tStack *s, tToken *t);
-void commandList(tStack *s, tToken *t);
-void function(tStack *s, tToken *t);
-void functionCall(tStack *s, tToken *t);
-void expressionSemicolon(tStack *s, tToken *t);
-void argumentArgumentList1(tStack *s, tToken *t);
-void paramList1(tStack *s, tToken *t);
-void paramList2(tStack *s, tToken *t);
-void program(tStack *s, tToken *t);
-void returnEnd(tStack *s, tToken *t);
-void returnExpression(tStack *s, tToken *t);
-void statement(tStack *s, tToken *t);
-void statementFunction(tStack *s, tToken *t);
-void statementCommand(tStack *s, tToken *t);
-void statementEnd(tStack *s, tToken *t);
-void expression(tStack *s, tToken *t);
+void argumentString(tStack **s, tToken *t);
+void argumentInteger(tStack **s, tToken *t);
+void argumentVar(tStack **s, tToken *t);
+void argumentNull(tStack **s, tToken *t);
+void argumentBoolean(tStack **s, tToken *t);
+void argumentDouble(tStack **s, tToken *t);
+void semicolon(tStack **s, tToken *t);
+void closebrace(tStack **s, tToken *t);
+void closeparen(tStack **s, tToken *t);
+void argumentList(tStack **s, tToken *t);
+void argumentArgumentList1(tStack **s, tToken *t);
+void argumentArgumentList2(tStack **s, tToken *t);
+void block(tStack **s, tToken *t);
+void commandVar(tStack **s, tToken *t);
+void commandReturn(tStack **s, tToken *t);
+void commandIf(tStack **s, tToken *t);
+void commandWhile(tStack **s, tToken *t);
+void commandList(tStack **s, tToken *t);
+void function(tStack **s, tToken *t);
+void functionCall(tStack **s, tToken *t);
+void expressionSemicolon(tStack **s, tToken *t);
+void argumentArgumentList1(tStack **s, tToken *t);
+void paramList1(tStack **s, tToken *t);
+void paramList2(tStack **s, tToken *t);
+void program(tStack **s, tToken *t);
+void returnEnd(tStack **s, tToken *t);
+void returnExpression(tStack **s, tToken *t);
+void statement(tStack **s, tToken *t);
+void statementFunction(tStack **s, tToken *t);
+void statementCommand(tStack **s, tToken *t);
+void statementEnd(tStack **s, tToken *t);
+void expression(tStack **s, tToken *t);
+void expressionParen(tStack **s, tToken *t);
 
-void stackInit(tStack *s, int size);
-int stackPop(tStack *s);
-void stackPush(tStack *s, int data);
-int stackTop(tStack *s);
-int stackTopTerminal(tStack *s, tStack *tmpStack, bool useTmpStack);
-int stackEmpty(tStack *s);
-void printStack(tStack *s);
+void reduce(tStack **s,tStack **tmpStack);
+void shift(tStack **s, tStack **tmpStack, tToken *t);
+void equalsign(tStack **s, tStack **tmpStack,tToken *t);
 
-void reduce(tStack *s,tStack *tmpStack);
-void shift(tStack *s, tStack *tmpStack, tToken *t);
-void equalsign(tStack *s, tStack *tmpStack,tToken *t);
-
-#define SHOW_DEBUG
-
-#ifndef SHOW_DEBUG
-	#define DEBUG(message);
-#else
-	#define DEBUG(message) printf("%s: %s\n", __PRETTY_FUNCTION__, message);
-#endif
+typedef void (LLFunction)(tStack **, tToken*); //TODO nepoužíváme nikde token, odebrat
 
 sFunction * actualFunction[2];
 sFunction * functionTree;
 extern FILE* f;
+extern int precedenceTable[][15];
+extern int LLTable[][36];
+extern LLFunction *LLRule[];
 
-typedef void (LLFunction)(tStack *, tToken*);
 
-int precedenceTable[][15] =
-{
-	{REDUCE,REDUCE,SHIFT,SHIFT,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,SHIFT,REDUCE,SHIFT,REDUCE},
-	{REDUCE,REDUCE,SHIFT,SHIFT,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,SHIFT,REDUCE,SHIFT,REDUCE},
-	{REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,SHIFT,REDUCE,SHIFT,REDUCE},
-	{REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,SHIFT,REDUCE,SHIFT,REDUCE},
-	{REDUCE,REDUCE,SHIFT,SHIFT,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,SHIFT,REDUCE,SHIFT,REDUCE},
-	{SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,SHIFT,REDUCE,SHIFT,REDUCE},
-	{SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,SHIFT,REDUCE,SHIFT,REDUCE},
-	{SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,SHIFT,REDUCE,SHIFT,REDUCE},
-	{SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,SHIFT,REDUCE,SHIFT,REDUCE},
-	{SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,REDUCE,REDUCE,SHIFT,REDUCE,SHIFT,REDUCE},
-	{SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,REDUCE,REDUCE,SHIFT,REDUCE,SHIFT,REDUCE},
-	{SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,EQUALSIGN,SHIFT,NOTHING},
-	{REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,NOTHING,REDUCE,NOTHING,REDUCE},
-	{REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,REDUCE,NOTHING,REDUCE,NOTHING,REDUCE},
-	{SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,SHIFT,NOTHING,SHIFT,NOTHING}
-};
-
-LLFunction *LLRule[] =
-{
-	NULL,
-	argumentString,
-	argumentInteger,
-	argumentVar,
-	argumentNull,
-	argumentBoolean,
-	argumentDouble,
-	argumentList,
-	argumentArgumentList1,
-	argumentArgumentList2,
-	argumentList,
-	block,
-	semicolon,
-	commandVar,
-	commandReturn,
-	commandIf,
-	commandWhile,
-	closebrace,
-	commandList,
-	function,
-	functionCall,
-	expressionSemicolon,
-	closeparen,
-	paramList1,
-	closeparen,
-	paramList2,
-	program,
-	returnEnd,
-	returnExpression,
-	statementCommand,
-	statementFunction,
-	statementEnd,
-	expression
-};
-
-/* @var tabulka která vrací pravidlo, to se použije pro indexaci funkcí LL tabulky */
-int LLTable[][36] =
-{
-	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,5,2,6,1,4,0,0,0,0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0,0,0,7,0,0,0,8,8,8,8,8,8,0,0,0,0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0,0,0,10,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,9,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,11,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,12,0,13,0,0,0,0,0,16,14,15,0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,18,0,18,0,0,0,0,0,18,18,18,0,0,0,0,0,17,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,19,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0,0,21,0,0,0,20,21,21,21,21,21,21,0,0,0,0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0,0,0,22,0,0,0,23,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0,0,0,24,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,25,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,26,0},
-	{0,0,0,0,0,0,0,0,0,0,0,28,0,0,27,0,28,28,28,28,28,28,0,0,0,0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,29,0,29,0,0,0,0,0,29,29,29,0,30,0,0,0,0,0,0,0,31},
-	{0,0,0,0,0,0,0,0,0,0,0,32,0,0,0,0,32,32,32,32,32,32,0,0,0,0,0,0,0,0,0,0,0,0,0}
-};
-
-char* tokenNames[256] = {
-	[BEGIN] = "BEGIN",
-	[END] = "END",
-	[NULLV] = "NULLV",
-	[BOOLEAN] = "BOOLEAN",
-	[WHILE] = "WHILE",
-	[RETURN] = "RETURN",
-	[IF] = "IF",
-	[ELSE] = "ELSE",
-	[FUNCTION] = "FUNCTION",
-
-	[PLUS] = "PLUS",
-	[MINUS] = "MINUS",
-	[TIMES] = "TIMES",
-	[DIVISION] = "DIVISION",
-
-	[BIGGER] = "BIGGER",
-	[LESSER] = "LESSER",
-	[BIGGEREQUAL] = "BIGGEREQUAL",
-	[LESSEREQUAL] = "LESSEREQUAL",
-	[EQUAL] = "EQUAL",
-	[NOTEQUAL] = "NOTEQUAL",
-
-	[ASSIGN] = "ASSIGN",
-
-	[ID] = "ID",
-	[INTEGER] = "INTEGER",
-	[DOUBLE] = "DOUBLE",
-	[STRING] = "STRING",
-	[VAR] = "VAR",
-	[VALUE] = "VALUE",
-	[E] = "E",
-
-	[SEMICOLON] = "SEMICOLON",
-	[DOT] = "DOT",
-	[COMMA] = "COMMA",
-	[OPENPAREN] = "OPENPAREN",
-	[CLOSEPAREN] = "CLOSEPAREN",
-	[OPENBRACE] = "OPENBRACE",
-	[CLOSEBRACE] = "CLOSEBRACE",
-
-	[INVALIDCHAR] = "INVALIDCHAR",
-	[NARGUMENT] = "NARGUMENT",
-	[NARGUMENTLIST1] = "NARGUMENTLIST1",
-	[NARGUMENTLIST2] = "NARGUMENTLIST2",
-	[NBLOCK] = "NBLOCK",
-	[NCOMMAND] = "NCOMMAND",
-	[NCOMMANDLIST] = "NCOMMANDLIST",
-	[NFUNCTION] = "NFUNCTION",
-	[NFUNCTIONCALLEXPRESSION] = "NFUNCTIONCALLEXPRESSION",
-	[NPARAMLIST1] = "NPARAMLIST1",
-	[NPARAMLIST2] = "NPARAMLIST2",
-	[NPROGRAM] = "NPROGRAM",
-	[NRETURN] = "NRETURN",
-	[NSTATEMENT] = "NSTATEMENT",
-	[NEXPRESSION] = "NEXPRESSION",
-	[NOTHING] = "NOTHING",
-	[REDUCE] = "REDUCE",
-	[SHIFT] = "SHIFT",
-	[EQUALSIGN] = "EQUALSIGN",
-};
 
 #endif
 
